f2e890a5cd1ef962fc3194fb26da454d3da13d73
Berkant3737 <48642168+Berkant3737@users.noreply.github.com>
2022-11-27 23:02:52 +0300

    Cross page update

--- src/containers/PostDetails/PostDetails.jsx
+++ src/containers/PostDetails/PostDetails.jsx
@@ -1,72 +1,83 @@
-import React from 'react'
-import logo from "../../images/avatar-icon.jpg"
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import SearchBar from "../SearchBar/SearchBar";
+import RecentMessages from "../RecentMessages/RecentMessages";
+import Footer from "../Footer/Footer";
+import logo from "../../images/avatar-icon.jpg";
+import { FaHashtag, FaClock,FaHeart } from "react-icons/fa";
+import { BsChat,BsEye} from "react-icons/bs";
+import { FiRefreshCw } from "react-icons/fi";
 function PostDetails() {
   return (
     <div>
-      
-      <div className="comment-line pt-5 text-white">
-        <div className="container">
-            <div className="comment-line-title p-3">
-                <i className="fa-solid fa-arrows-rotate text-success"></i><a href="$" className="text-muted fw-bold mx-2">Yenile</a>
-            </div>
-            <div className="comment-line-content p-4">
-                <div className="row">
-                    <div className="col-1 mx-auto">
-                        <img src={logo} className="p-2 p-lg-0" alt="" />
+      <Navbar />
+      <SearchBar />
+      <div className="container mt-4">
+            <div className="row post-details">
+                {/* HEADER */}
+                    <div className="col-12 recent-messages-header d-flex align-items-center">
+                        <div className="col-6 d-flex align-items-center">
+                            <i><FiRefreshCw/> </i>
+                            <h6 className='fw-bold ms-2 my-0'>Yenile</h6>
+                        </div>
+                        <div className="col-6 d-inline-flex justify-content-around d-none">
+                            <h6 className='fw-bold ms-0 my-0'>Konu</h6>
+                            <h6 className='fw-bold ms-0 my-0'>Beğeni</h6>
+                            <h6 className='fw-bold ms-0 my-0'>Cevap</h6>
+                        </div>
                     </div>
-                    <div className="col-10 col-lg-6 ms-4 ms-lg-0">
-                        <h5 className="fw-bold px-2"><a href="#" className="text-white">One page template</a></h5>
-                        <div className="row text-muted fw-bold">
-                            <p className="col-6 col-lg-3"><i className="fa-solid fa-hashtag p-2"></i> General </p>
-                            <p className="col-6"><i className="fa-regular fa-clock p-2"></i> 25 minutes ago</p>
-                        </div>                        
+                 {/* HEADER */}
+                {/* BODY */}
+                    <div className="col-12 post-details-body d-flex align-items-center">
+                        <div className="col-12 col-lg-6 d-inline-block px-2 py-3">
+                            <div className="d-flex align-items-center">
+                                <img src={logo} alt="" />
+                                <a href="/userpost"><h6 className='fw-normal ps-3 mb-0 pb-0'>SIFIRDAN JAVASCRİPT DERSLERİ</h6></a>
+                                </div>
+                            </div>
+                            <div className="col-12 col-lg-6 d-none d-lg-inline-flex justify-content-around">
+                              <h6 className='w-25 text-center'><i><FaHeart/></i> 10</h6>
+                              <h6 className='w-25 text-center'><i><BsChat/></i> 10</h6>
+                              <h6 className='w-25 text-center'><i><BsEye/></i> 10</h6>
+                        </div>
                     </div>
-                 
-                    <div className="col-10 col-lg-3 d-flex align-items-center ms-auto">
-                        <p className="mx-auto"><i className="fa-solid fa-heart p-2 mx-auto"></i> 10 </p>
-                        <p className="mx-auto"><i className="fa-regular fa-comment p-2 mx-auto"></i> 70</p>
-                        <p className="mx-auto"><i className="fa-regular fa-eye p-2 mx-auto"></i> 420</p>
-                    </div>
-                </div>
-            </div>
-            <div className="comment-line-content p-4">
-                <div className="row">
-                    <div className="col-1 mx-auto">
-                        <img src={logo} className="p-2 p-lg-0" alt="" />
-                    </div>
-                    <div className="col-10 col-lg-6 ms-4 ms-lg-0">
-                        <h5 className="fw-bold px-2"><a href="#" className="text-white">Support "IntelliSense" plugin for JetBrains WebStorm</a></h5>
-                        <div className="row text-muted fw-bold">
-                            <p className="col-6 col-lg-3"><i className="fa-solid fa-hashtag p-2"></i> İdeas </p>
-                            <p className="col-6"><i className="fa-regular fa-clock p-2"></i> 2h ago</p>
-                        </div>                        
-                    </div>
-                    <div className="col-10 col-lg-3 d-flex align-items-center ms-auto">
-                        <p className="mx-auto"><i className="fa-solid fa-heart p-2 mx-auto"></i> 50 </p>
-                        <p className="mx-auto"><i className="fa-regular fa-comment p-2 mx-auto"></i> 700</p>
-                        <p className="mx-auto"><i className="fa-regular fa-eye p-2 mx-auto"></i> 1420</p>
-                    </div>
-                </div>
+                {/* BODY */}
+                
             </div>
         </div>
-        <p className="text-center mt-4">Total: <span>25</span></p>
-        <nav className="mt-4" aria-label="Page navigation example">
-            <ul className="pagination justify-content-center">
-              <li className="page-item disabled">
-                <a className="page-link">Previous</a>
-              </li>
-              <li className="page-item"><a className="page-link" href="#">1</a></li>
-              <li className="page-item"><a className="page-link active" href="#">2</a></li>
-              <li className="page-item"><a className="page-link" href="#">3</a></li>
-              <li className="page-item">
-                <a className="page-link" href="#">Next</a>
-              </li>
-            </ul>
-          </nav>
-    </div> 
 
+      {/* Pagination */}
+      <nav className="mt-4" aria-label="Page navigation example">
+        <ul className="pagination justify-content-center">
+          <li className="page-item">
+            <a className="page-link">Önceki</a>
+          </li>
+          <li className="page-item">
+            <a className="page-link active" href="#">
+              1
+            </a>
+          </li>
+          <li className="page-item">
+            <a className="page-link " href="#">
+              2
+            </a>
+          </li>
+          <li className="page-item">
+            <a className="page-link" href="#">
+              3
+            </a>
+          </li>
+          <li className="page-item">
+            <a className="page-link" href="#">
+              Sonraki
+            </a>
+          </li>
+        </ul>
+      </nav>
+      {/* Pagination */}
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default PostDetails
+export default PostDetails;
